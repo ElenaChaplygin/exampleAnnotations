062769fd2b30a6abece5bd1955968525f8e525c0
elena chaplygina <atma.mangalam@gmail.com>
2019-05-18 20:06:54 +0300

    exampleAnnotations initial

---
+++ src/exampleAnnotations/AnnotationOrder.java
@@ -0,0 +1,76 @@
+package exampleAnnotations;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.ITest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class AnnotationOrder {
+	// test case 1
+	// parameters inside annotation says that we want to run this test with 3 threads
+	   @Test (threadPoolSize=3, invocationCount=3, timeOut=1000)
+	   public void testCase1() {
+	      System.out.println("in test case 1 |" + Thread.currentThread().getId());
+	   }
+
+	   // test case 2
+	   @Test
+	   public void testCase2() {
+	      System.out.println("in test case 2 |" + + Thread.currentThread().getId());
+	   }
+	   
+	// test case 3
+	   @Test
+	   public void testCase3() {
+	      System.out.println("in test case 3 |" + + Thread.currentThread().getId());
+	   }
+
+
+	/*   @BeforeMethod
+	   public void beforeMethod() {
+	      System.out.println("in beforeMethod");
+	   }
+
+	   @AfterMethod
+	   public void afterMethod() {
+	      System.out.println("in afterMethod");
+	   }
+
+	   @BeforeClass
+	   public void beforeClass() {
+	      System.out.println("in beforeClass");
+	   }
+
+	   @AfterClass
+	   public void afterClass() {
+	      System.out.println("in afterClass");
+	   }
+
+	   @BeforeTest
+	   public void beforeTest() {
+	      System.out.println("in beforeTest");
+	   }
+
+	   @AfterTest
+	   public void afterTest() {
+	      System.out.println("in afterTest");
+	   }
+
+	   @BeforeSuite
+	   public void beforeSuite() {
+	      System.out.println("in beforeSuite");
+	   }
+
+	   @AfterSuite
+	   public void afterSuite() {
+	      System.out.println("in afterSuite");
+	   }
+*/
+
+}
